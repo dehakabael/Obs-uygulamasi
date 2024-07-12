91f90ff92bd94c1f7cd1cab5bdaf367af568c6af
Deha Kabael <dkabael@gmail.com>
2024-07-12 16:58:23 +0300

    first commit

---
+++ ogrencıuygulaması/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace ogrencıuygulaması
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool kontrol = true;
+            ogrencı öğrenci1 = new ogrencı(230910082, "Deha", "Kabael", 70, 55, 55, "Ufuküniversitesi");
+
+
+            Console.WriteLine("Obs uygulamsına hoş geldiniz");
+            Console.WriteLine("Yapmak istediğiniz işlemi seçin");
+
+            Console.WriteLine("1- Öğrencinin bilgilerini göster 2- Öğrencinin Ortalamasını göster 3- Öğrencinin Okulunu göster 4-Çıkış yap");
+
+            while (kontrol) {
+
+                string secım = Console.ReadLine();
+
+                switch (secım)
+                {
+                    case "1":
+                        öğrenci1.ogrencibilgilerinigöster();
+                        break;
+                    case "2":
+                        double ogrenciortlama = öğrenci1.ogrencıortalamabul();
+                        Console.WriteLine(("Öğrencinin Ortalaması") + ogrenciortlama );
+                        break;
+
+                    case "3":
+                        öğrenci1.okulgetır();
+                        break;
+
+                    case "4":
+                        kontrol = false;
+                        break;
+
+
+
+                }
+
+            }
+            
+
+
+
+        }
+    }
+}
---
+++ ogrencıuygulaması/ogrencı.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ogrencıuygulaması
+{
+    internal class ogrencı
+    {
+        private int ogrencıno;
+        private string ısım;
+        private string soyısım;
+        private int vize1;
+        private int vize2;
+        private int fınal;
+        private string okulısmı;
+
+        public ogrencı(int ogrencıno, string ısım, string soyısım, int vize1, int vize2, int fınal, string okulısmı)
+        {
+            this.ogrencıno = ogrencıno;
+            this.ısım = ısım;
+            this.soyısım = soyısım;
+            this.vize1 = vize1;
+            this.vize2 = vize2;
+            this.fınal = fınal;
+            this.okulısmı = okulısmı;
+        }
+
+        public void ogrencibilgilerinigöster()
+        {
+            Console.WriteLine("Öğrenci numarası" + ogrencıno);
+            Console.WriteLine("Öğrenci ismi " + ısım);
+            Console.WriteLine("Öğrenci soyadı " + soyısım);
+            Console.WriteLine("Öğrenci vize 1 " + vize1);
+            Console.WriteLine("Öğrenci vize 2 " + vize2);
+            Console.WriteLine("Öğrenci final " + fınal);
+            Console.WriteLine(("Öğrenci okulun ismi ") + okulısmı);
+
+
+        }
+
+        public double ogrencıortalamabul()
+        {
+          double ortalama = vize1*0.2 + vize2*0.2 + fınal*0.6;
+            return ortalama;
+        }
+        
+        public void okulgetır()
+        {
+            Console.WriteLine(("Öğrencinin okuduğu ismi") + okulısmı);
+            return;
+        }
+
+    }
+}
